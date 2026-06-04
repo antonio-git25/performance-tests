7c92e33192678573e8f07b041c6841b9be0ff46b
antonio-git25 <anton.lutsenko1990@yandex.ru>
2026-06-04 22:18:00 +0300

    add requrements and readme.md

--- main.py
+++
@@ -1,7 +0,0 @@
-import httpx
-
-def request_hook(request: httpx.Request):
-    print(f"Запрос: {request.method} {request.url}")
-
-client = httpx.Client(event_hooks={"request": [request_hook]})
-response = client.get("https://example.com")
